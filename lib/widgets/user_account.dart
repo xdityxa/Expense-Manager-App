7c926cd52d39fe53a7feae900054ec02a7db9f46
Aditya Gupta <aditya.gupta181099@gmail.com>
2020-05-10 23:38:48 +0530

    minor updates

---
+++ lib/widgets/user_account.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class UserAccount extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+      ),
+    );
+  }
+}
